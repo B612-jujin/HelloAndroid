7e143c3d207beca923a84985c7b6460f62847f35
B612-jujin <jujingood@gmail.com>
2025-03-17 16:27:48 +0900

    안드로이드 스튜디오에서 텍스트 입력 받은 값 처리에 대한 이벤트 생성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
-    implementation(libs.constraintlayout)
+    implementation(libs.constraintlayout)//외부 의존성 추가
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/kr/ac/kopo/helloandroid/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/helloandroid/MainActivity.java
@@ -1,6 +1,11 @@
 package kr.ac.kopo.helloandroid;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +19,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_main); //xml에 작성된 뷰를 화면에 띄우는 메서드라고 생각하면 된다. 가장 위에 있어줘야 한다.
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        TextView text1 = (TextView) findViewById(R.id.text1);
+        EditText editText = (EditText) findViewById(R.id.NameTextText);
+
+
+
+
+        //리스너 설정 -> 클릭했을 때 호출되는 일이 일어난다.
+
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            int TextMsg = 0;
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "버튼을 클릭하셨습니다.", Toast.LENGTH_SHORT);
+                toast.show();
+                if(TextMsg ==0){
+                    text1.setText(editText.getText().toString());
+                    TextMsg = 1;
+                }
+                if(TextMsg == 1 || editText.getFreezesText()) {
+                    text1.setText("None");
+                    TextMsg = 0;
+                }
+
+            }
         });
+
+
+
+
+
+
     }
 }
